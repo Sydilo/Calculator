2ca20f20784faa99f58e3356cb57a59df6243437
Yura Sydilo <yurasydilo@MacBook-Pro-Yura.local>
2022-10-21 08:59:04 +0300

    Initial commit

---
+++ Calculator.py
@@ -0,0 +1,15 @@
+while True:
+    number_1 = int(input("Enter your first number:"))
+    number_2 = int(input("Enter your second number:"))
+
+    print("Sum: ".format(number_1, number_2))
+    print(number_1 + number_2)
+
+    print("Difference: ".format(number_1, number_2))
+    print(number_1 - number_2)
+
+    print("Product ".format(number_1, number_2))
+    print(number_1 * number_2)
+
+    print("Division: ".format(number_1, number_2))
+    print(number_1 / number_2)
